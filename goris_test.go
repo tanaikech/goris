2c3c03a143564784c13d1a6c01f8f791c8453632
tanaikech <tanaike@hotmail.com>
2017-06-13 09:57:15 +0900

    modified

--- goris_test.go
+++ goris_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestImgFromURL(t *testing.T) {
 	imageurl := "https://github.com/tanaikech/goris/blob/master/myavatar.png?raw=true"
-	webpages := true
+	webpages := false
 	results := ris.DefImg(webpages).ImgFromURL(imageurl)
 	t.Log(results)
 }
